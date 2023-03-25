fa235e5f9e2c31bdfd468e28a5d9e115c5f8d2a4
Yangxiaoge Zhang <yzhang433@vlab20.cs.usfca.edu>
2023-03-25 01:59:56 -0700

    Compression checked

--- client.c
+++ client.c
@@ -7,29 +7,74 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <fcntl.h>
+
 #include "debug.h"
 #include "parser.h"
 #include <errno.h>
 
 #define SIZE 1024
 
-void bindSocket(int sockfd, struct addrinfo* res) {
-    int bd = bind(sockfd, res->ai_addr, res->ai_addrlen);
+int bindSocket(int sockfd, struct sockaddr* addr, int addrlen) {
+    int bd = bind(sockfd, addr, addrlen);
     if (bd < 0) {
         perror("Bind error.");
         exit(1);
     }
+    return bd;
 }
 
-// void sendUDP(int sockfd) {
-//     if (sendto(socket_desc, server_message, strlen(server_message), 0,
-//          (struct sockaddr*)&client_addr, client_struct_length) < 0){
-//         printf("Can't send\n");
-//         return -1;
-//     }
-//     1
-// }
+void createRandomSeq(uint8_t *payload, int bufsize) {
+    int fd;
+    
+    fd = open("/dev/urandom", O_RDONLY);
+    if (fd < 0) {
+        perror("Open error");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (read(fd, payload, bufsize) < 0) {
+        perror("Read error");
+        exit(EXIT_FAILURE);
+    }
+    
+    close(fd);
+}
 
+uint8_t* create_payload(uint16_t id, size_t payload_size, int randomOrNot) {
+    if (payload_size < sizeof(id)) {
+        printf("Packet size should be at least 2 bytes for the ID.\n");
+        return NULL;
+    }
+
+    uint8_t* payload = (uint8_t*)calloc(payload_size, sizeof(uint8_t));
+    if (payload == NULL) {
+        printf("Memory allocation failed.\n");
+        return NULL;
+    }
+    if(randomOrNot == 1) {
+        createRandomSeq(payload, payload_size);
+    }
+    payload[0] = (id >> 8) & 0xFF; // Store the high byte of the ID
+    payload[1] = id & 0xFF;        // Store the low byte of the ID
+    // for (size_t i = 0; i < payload_size; i++) {
+    //     printf("%u", (unsigned int)payload[i]);
+    // }
+    return payload;
+}
+
+void sendUdp(ConfigInfo *cfg, int sockfd, struct sockaddr_in *addr, int sizeAddr, int randomOrNot)
+{
+    for(int i = 0; i < cfg->num_udp_packet; i++) {
+        uint8_t* payload = create_payload((uint16_t)i, cfg->size_udp, randomOrNot);
+        if (sendto(sockfd, payload, cfg->size_udp, 0, (struct sockaddr*)addr, sizeAddr) < 0){
+            printf("Can't send\n");
+            exit(1);
+        }
+        // LOG("%u\n", payload[1]);
+        free(payload);
+    }
+}
 
 int main() {
     char *buffer = read_file("config.json");
@@ -39,22 +84,22 @@ int main() {
     memset(&cfg, 0, sizeof(cfg));
     parseJSON(copy_buf, &cfg);
 
-    struct sockaddr_in my_addr;
-    memset(&my_addr, 0, sizeof(my_addr));
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_addr.s_addr = INADDR_ANY;
-    my_addr.sin_port = htons(8080);
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("10.10.12.99");
+    server_addr.sin_port = htons(8080);
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("Socket fails.");
         exit(1);
     }
-    socklen_t sl = sizeof(my_addr);
-    if (connect(sockfd, (struct sockaddr*)&my_addr, sl) < 0) {
+    socklen_t sl = sizeof(server_addr);
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sl) < 0) {
         perror("Connection failed.");
         exit(0);
     }
-    LOG("%i connected\n", sockfd);
     
     int sd = send(sockfd, buffer, strlen(buffer), 0);
     if (sd == -1) {
@@ -62,37 +107,55 @@ int main() {
       exit(1);
     } 
     close(sockfd);
-  
-    // freeaddrinfo(res);
 
-    // char hostname[256];
-    // int result = gethostname(hostname, sizeof(hostname));
-    // if (result != 0) {
-    //     perror("Gethosename fails.");
-    //     exit(1);
-    // }
-    my_addr.sin_addr.s_addr = inet_addr(cfg.ip);
-    my_addr.sin_port = htons(atoi(cfg.dst_port_udp));
+    // UDP starts below
+    struct sockaddr_in udp_addr;
+    memset(&udp_addr, 0, sizeof(udp_addr));
+    udp_addr.sin_family = AF_INET;
+    udp_addr.sin_addr.s_addr = inet_addr(cfg.ip);
+    udp_addr.sin_port = htons(atoi(cfg.dst_port_udp));
     int udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    char *buf = "aloha";
-    if (sendto(udp_sockfd, buf, strlen(buf), 0,
-        (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0){
-        printf("Can't send\n");
-        return -1;
-    }
-// //     struct addrinfo *udp_res;
-// //     int udp_status = getaddrinfo(hostname, "9876", &hints, &udp_res);
-// //     if (udp_status < 0) {
-// //         perror("Get addrinfo fails udp.");
-// //         return 1;
-// //     }
-// //     bindSocket(sockfd, udp_res);
     
-// //     // int val = IP_PMTUDISC_DO;
-// //     // setsockopt(sockfd, IPPROTO_IP, IP_MTU_DISCOVER, &val, sizeof(val));
+    int val = IP_PMTUDISC_DO;
+    server_addr.sin_port = htons(atoi(cfg.src_port_udp));
+    // bindSocket(udp_sockfd, (struct sockaddr*)&client_addr, sizeof(server_addr));
+    setsockopt(udp_sockfd, IPPROTO_IP, IP_MTU_DISCOVER, &val, sizeof(val));
 
-//     free(buffer);
-//     free(copy_buf);
-    // freeConfig(&cfg);
-    // close(sockfd);
+    sendUdp(&cfg, udp_sockfd, &udp_addr, sizeof(udp_addr), 0);
+    sleep(15);
+    sendUdp(&cfg, udp_sockfd, &udp_addr, sizeof(udp_addr), 1);
+    close(udp_sockfd);
+
+    // Phase3
+    sleep(20);
+    // struct sockaddr_in p3_addr;
+    // memset(&p3_addr, 0, sizeof(p3_addr));
+    // p3_addr.sin_family = AF_INET;
+    // p3_addr.sin_addr.s_addr = inet_addr(cfg.ip);
+    // p3_addr.sin_port = htons(atoi(cfg.dst_port_udp));
+    int p3_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (p3_sockfd < 0) {
+        perror("Socket fails.");
+        exit(1);
+    }
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("10.10.12.99");
+    server_addr.sin_port = htons(8765);
+    int server_fd = connect(p3_sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (server_fd < 0) {
+        perror("Connection failed.");
+        exit(0);
+    }
+
+    char check = '0';
+    int receive = recv(p3_sockfd, &check, 1, 0);
+    if (receive < 0){
+      exit(1);
+    }
+    LOG("%c\n", check);
+    if(check == '0') printf("No compression was detected.\n");
+    else printf("Compression detected!\n");
+    close(p3_sockfd);
+    free(copy_buf);
 }
--- server.c
+++ server.c
@@ -8,7 +8,8 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
+#include <sys/time.h>
+#include <math.h>
 #include <errno.h>
 #include "debug.h"
 #include "parser.h"
@@ -60,30 +61,84 @@ int acceptSocket(int sockfd, struct sockaddr* my_addr, int addrlen) {
     return new_fd;
 }
 
+int setTimeout(int sockfd) {
+    struct timeval timeout;
+    timeout.tv_sec = 10;
+    timeout.tv_usec = 0;
+    int output = setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout));
+    if (output < 0) {
+        perror("Setsockopt error");
+        exit(EXIT_FAILURE);
+    }
+    return output;
+}
+
+double recordTime(int sockfd, struct sockaddr_in *client_addr, socklen_t client_struct_length, ConfigInfo *cfg) {
+    struct timeval start, end;
+    double elapsed_time;
+    memset(&start, 0, sizeof(start));
+    memset(&start, 0, sizeof(start));
+    setTimeout(sockfd);
+    for (int i = 0; i < cfg->num_udp_packet; i++) {
+        uint8_t udp_packet[1000];
+        int bytes_received = recvfrom(sockfd, udp_packet, cfg->size_udp, 0, (struct sockaddr*)client_addr, &client_struct_length);
+        if (bytes_received < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                printf("Timeout occurred\n");
+                LOG("%s\n", "timeout");
+                break;
+            } else {
+                // LOG("%s\n", "boom.");
+                perror("Recvfrom error");
+                exit(1);
+            }
+        } else {
+            if (start.tv_sec == 0 && start.tv_usec == 0) {
+                gettimeofday(&start, NULL);
+            }
+            gettimeofday(&end, NULL);
+        }
+    }
+    elapsed_time = (end.tv_sec - start.tv_sec) + ((end.tv_usec - start.tv_usec) / 1000000.0);
+    LOG("Elapsed time: %f seconds\n", elapsed_time);
+    // LOG("Span time: %f seconds\n", span);
+    if(elapsed_time + 10 < cfg->inter_time) {
+        sleep(cfg->inter_time - 10 - elapsed_time);
+    }
+    return elapsed_time;
+}
+
 
 
 
 int main(void) {
     int sockfd;
-    struct sockaddr_in my_addr;
-    memset(&my_addr, 0, sizeof(my_addr));
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_addr.s_addr = INADDR_ANY;
-    my_addr.sin_port = htons(8080);
+    struct sockaddr_in server_addr, client_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("10.10.12.99");
+    server_addr.sin_port = htons(8080);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("Socket fails.");
         exit(1);
     }
-    int bs = bindSocket(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr));
+    int reuse_addr = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse_addr, sizeof(reuse_addr)) < 0) {
+        perror("setsockopt");
+        close(sockfd);
+        exit(1);
+    }
+    int bs = bindSocket(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
     int sockls = listenSocket(sockfd);
-    int new_fd = acceptSocket(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr));
-    char buffer[SIZE];
+    int new_fd = acceptSocket(sockfd, (struct sockaddr*)&client_addr, sizeof(client_addr));
+    char *buffer = malloc(1024);
     int receive = recv(new_fd, buffer, SIZE, 0);
     if (receive <= 0){
       exit(1);
     }
     LOG("%s\n", buffer);
+    close(new_fd);
     close(sockfd);
 
 
@@ -91,23 +146,50 @@ int main(void) {
     ConfigInfo cfg;
     memset(&cfg, 0, sizeof(cfg));
     parseJSON(buffer, &cfg);
-    my_addr.sin_addr.s_addr = inet_addr(cfg.ip);
-    my_addr.sin_port = htons(atoi(cfg.dst_port_udp));
-    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    LOG("%s\n", "2");
-    bindSocket(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr));
-    char udp_packet[SIZE];
-    struct sockaddr_in client_addr;
+    struct sockaddr_in udp_addr;
+    memset(&udp_addr, 0, sizeof(udp_addr));
+    udp_addr.sin_family = AF_INET;
+    udp_addr.sin_addr.s_addr = INADDR_ANY;
+    udp_addr.sin_port = htons(atoi(cfg.dst_port_udp));
+    int new_sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    bindSocket(new_sockfd, (struct sockaddr*)&udp_addr, sizeof(udp_addr));
+    // struct sockaddr_in client_addr;
     memset(&client_addr, 0, sizeof(client_addr));
     socklen_t client_struct_length = sizeof(client_addr);
-    if (recvfrom(sockfd, udp_packet, sizeof(udp_packet), 0,
-         (struct sockaddr*)&client_addr, &client_struct_length) < 0){
-        printf("Couldn't receive\n");
-        fprintf(stderr, "some_function failed with error %d: %s\n", errno, strerror(errno));
-        return -1;
-    }
-    LOG("%s\n", udp_packet);
+
+    double gap1 = recordTime(new_sockfd, &client_addr, client_struct_length, &cfg);
+    double gap2 = recordTime(new_sockfd, &client_addr, client_struct_length, &cfg);
+    double result = gap2 - gap1;
     // free(cfg);
+    close(new_sockfd);
+    free(buffer);
+
+    // struct sockaddr_in p3_addr;
+    // memset(&p3_addr, 0, sizeof(p3_addr));
+    // p3_addr.sin_family = AF_INET;
+    // p3_addr.sin_addr.s_addr = inet_addr(cfg.ip);
+    // p3_addr.sin_port = htons(atoi(cfg.dst_port_udp));
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket fails.");
+        exit(1);
+    }
+    server_addr.sin_port = htons(8765);
+    bs = bindSocket(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    LOGP("Listening...\n");
+    sockls = listenSocket(sockfd);
+    memset(&client_addr, 0, sizeof(client_addr));
+    new_fd = acceptSocket(sockfd, (struct sockaddr*)&client_addr, sizeof(client_addr));
+    char check = '0';
+    if (fabs(result) > 0.1) {
+        check = '1';
+    }
+    int sd = send(new_fd, &check, 1, 0);
+    if (sd == -1) {
+        perror("[-]Error in sending file.");
+        exit(1);
+    }
+    close(new_fd);
     close(sockfd);
     return 0;
 }
